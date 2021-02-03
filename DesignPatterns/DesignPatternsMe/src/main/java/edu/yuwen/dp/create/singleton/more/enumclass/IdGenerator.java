6352e0165a47d2710973026be29946c28341ad6b
yuwen <yuwen3@asiainfo.com>
2021-02-03 14:10:41 +0800

    1.单例模式的变种，多例模式，通过枚举值的数量，限制多例模式的数量

--- DesignPatterns/DesignPatternsMe/src/main/java/edu/yuwen/dp/create/singleton/more/enumclass/IdGenerator.java
+++ DesignPatterns/DesignPatternsMe/src/main/java/edu/yuwen/dp/create/singleton/more/enumclass/IdGenerator.java
@@ -1,16 +1,16 @@
-package edu.yuwen.dp.create.singleton.enumclass;
+package edu.yuwen.dp.create.singleton.more.enumclass;
 
 import java.util.concurrent.atomic.AtomicLong;
 
 /**
- * 1.单例模式
+ * 1.单例模式的变种，多例模式，通过枚举值的数量，限制多例模式的数量
  * 
- * 5.枚举
+ * 5.枚举  有多个有限的枚举值
  *
  * Demo:唯一递增ID号码生成器
  */
 public enum IdGenerator {
-    INSTANCE;
+    INSTANCE1, INSTANCE2, INSTANCE3;
 
     private AtomicLong id = new AtomicLong(0);
 
