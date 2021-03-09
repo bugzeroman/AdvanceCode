c5b493f6bb81aeaa53725b0266a2df7b553ff15b
yuwen <yuwen3@asiainfo.com>
2021-03-09 10:34:45 +0800

    8.备忘录者模式（行为型）  Demo: 可以撤回上一次添加的文本

---
+++ DesignPatterns/DesignPatternsMe/src/main/java/edu/yuwen/dp/behavior/memento/InputText.java
@@ -0,0 +1,26 @@
+package edu.yuwen.dp.behavior.memento;
+
+public class InputText {
+    private StringBuilder text = new StringBuilder();
+
+    public String getText() {
+        return text.toString();
+    }
+
+    public void append(String input) {
+        text.append(input);
+    }
+
+    public Snapshot createSnapshot() {
+        return new Snapshot(text.toString());
+    }
+
+    public void restoreSnapshot(Snapshot snapshot) {
+        this.text.replace(0, this.text.length(), snapshot.getText());
+    }
+
+    @Override
+    public String toString() {
+        return text.toString();
+    }
+}
---
+++ DesignPatterns/DesignPatternsMe/src/main/java/edu/yuwen/dp/behavior/memento/InputTextDemo.java
@@ -0,0 +1,29 @@
+package edu.yuwen.dp.behavior.memento;
+
+import java.util.Scanner;
+
+/**
+ * 8.备忘录者模式（行为型） 
+ *
+ * Demo: 分布添加文本，中间可以使用撤回上一次添加的文本
+ */
+public class InputTextDemo {
+    public static void main(String[] args) {
+        InputText inputText = new InputText();
+        SnapshotHolder snapshotsHolder = new SnapshotHolder();
+        Scanner scanner = new Scanner(System.in);
+        while (scanner.hasNext()) {
+            String input = scanner.next();
+            if (input.equals(":list")) {
+                System.out.println(inputText.toString());
+            } else if (input.equals(":undo")) {
+                Snapshot snapshot = snapshotsHolder.popSnapshot();
+                inputText.restoreSnapshot(snapshot);
+            } else {
+                snapshotsHolder.pushSnapshot(inputText.createSnapshot());
+                inputText.append(input);
+            }
+        }
+        scanner.close();
+    }
+}
